03e5ac865e1466e92cd590b842880bd9d3b766ce
rocskater <rocskater@gmail.com>
2021-10-26 11:37:29 -0500

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,39 @@
+// import React from 'react'
+import React, { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    
+    const [categories, setCategories] = useState(['One Punch']);
+    
+    // const handleAdd=() =>{
+    // //    setCategories( ['HunterXHunter',...categories]);
+    // setCategories (cats => [...cats,'HunterXHunter']);
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+
+            <AddCategory setCategories ={setCategories}/>
+            <hr/>
+
+            <ol>
+                { 
+                    categories.map(category =>(
+                        <GifGrid 
+                        key={category}
+                        category={category}/>
+                        // return <li key={category}>{category}</li>
+                    ))
+                }
+            </ol>
+
+           
+        </>
+    )
+
+}
